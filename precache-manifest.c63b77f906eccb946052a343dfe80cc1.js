8fe82e17d5b2ab16f67123b21bfef77bd54569c5
Saurabh0713 <109545639+Saurabh0713@users.noreply.github.com>
2023-03-18 19:11:21 +0530

    Updates

---
+++ precache-manifest.c63b77f906eccb946052a343dfe80cc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "640b227b3600a2bc82ba",
+    "url": "/word-counter-app/static/css/main.f5520e58.chunk.css"
+  },
+  {
+    "revision": "640b227b3600a2bc82ba",
+    "url": "/word-counter-app/static/js/main.0640986e.chunk.js"
+  },
+  {
+    "revision": "1414b9b9b7fbf553ef38",
+    "url": "/word-counter-app/static/js/runtime~main.7275c54d.js"
+  },
+  {
+    "revision": "c6c4b6c374b2d56df066",
+    "url": "/word-counter-app/static/css/2.8b23f020.chunk.css"
+  },
+  {
+    "revision": "c6c4b6c374b2d56df066",
+    "url": "/word-counter-app/static/js/2.afa57613.chunk.js"
+  },
+  {
+    "revision": "f4866491ae15624a43d3",
+    "url": "/word-counter-app/static/js/3.b700955a.chunk.js"
+  },
+  {
+    "revision": "4a2b3c934a2fd23f166be46f901b8c6c",
+    "url": "/word-counter-app/index.html"
+  }
+];
